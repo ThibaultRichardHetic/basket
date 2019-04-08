94be031227458b316c885cd49b00c3fc7c44e976
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2019-04-08 13:08:29 +0200

    :sparkles: add some init files and make a mvp model

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+
+// Configuration 
+  define('URL', 'http://localhost:8888/cours/basket/public/');
+
+// GET 'q' param
+$q = empty($_GET['q']) ? '' : $_GET['q'];
+
+
+// Define controller 
+$controller = '404';
+
+if($q == 'about-us')
+{
+  $controller = 'about';
+}
+else if ($q == '') {
+  $controller = 'home';
+}
+else if (preg_match('/^article\/[1-9][0-9]*$/', $q))
+{
+  $controller = 'article';
+}
+
+// Include controller
+include '../controllers/'.$controller.'.php';
