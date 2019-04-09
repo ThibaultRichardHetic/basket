d6c41c306b2aefd22eca84cdf5d1ffef2b58863b
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2019-04-09 12:23:09 +0200

    :construction: form handler wip

--- controllers/data/registration-handler.php
+++ controllers/data/registration-handler.php
@@ -36,20 +36,32 @@ if(!empty($_POST))
     {
         $messages['error'][] = "Il manque votre nom d'équipe";
     }
-    if(empty($team_name))
+    if(empty($mail))
     {
-        $messages['error'][] = "Il manque votre nom d'équipe";
+        $messages['error'][] = "Il manque votre adresse mail";
     }
-
     if(empty($password))
     {
-        $messages['error'][] = 'Missing password';
+        $messages['error'][] = 'Mot de passe manquant';
     }
     elseif (strlen($password) < 5) 
     {
-        $messages['error'][] = 'Password too short';
+        $messages['error'][] = 'Votre mot de passe est trop court';
+    }
+    if(empty($password_confirmation))
+    {
+        $messages['error'][] = "Votre conffirmation de mot de passe est vide";
     }
 
+    if( (!empty($password) && !empty($password_confirmation)) || ($password === $password_confirmation) )
+    {
+      $messages['success'][] = 'Mot de passe identique a la vérification';
+    }
+
+    if ( (!empty($password) && !empty($password_confirmation)) || ($password !== $password_confirmation) ) 
+    {
+      $messages['error'][] = "Vérification du mot de passe invalide";
+    }
 
 
     // Success
@@ -57,10 +69,22 @@ if(!empty($_POST))
     {
         $messages['success'][] = 'All good';
 
-        $_POST['login'] = '';
-        $_POST['password'] = '';
-        $_POST['age'] = '';
-        $_POST['gender'] = '';
+
+        $prepare = $pdo->prepare('INSERT INTO users (first_name, last_name, team_name, mail, password) VALUES (:content, :amount)');
+
+        $prepare->bindValue('first_name', $first_name);
+        $prepare->bindValue('last_name', $last_name);
+        $prepare->bindValue('team_name', $team_name);
+        $prepare->bindValue('mail', $mail);
+        $prepare->bindValue('password', $password);
+  
+
+        $prepare->execute();
+
+        $messages['success'][] = 'La dépense est enregistrée';
+
+        $_POST['content'] = '';
+        $_POST['amount'] = '';
     }
 }
 
