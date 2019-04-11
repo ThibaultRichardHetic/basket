3a40c68ac0a48540f59e5e41e1276120f40b2388
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2019-04-11 15:14:38 +0200

    :sparkles: creation league work for one email

--- controllers/data/league-handler.php
+++ controllers/data/league-handler.php
@@ -6,40 +6,164 @@ $messages = [
 'success' => [],
 ];
 
-$newUsers = array();
-array_push($newUsers,"blue","yellow");
-print_r($newUsers);
-
-
 if (!empty($_POST)) 
 {
-
   //Get variables 
   $league_name = $_POST['league_name'];
-  $friend_name_1 = $_POST['friend_name_1'];
+  $friend_mail_1 = $_POST['friend_mail_1'];
+  $friend_mail_2 = $_POST['friend_mail_2'];
+  $friend_mail_3 = $_POST['friend_mail_3'];
+  $friend_mail_4 = $_POST['friend_mail_4'];
+
+  $add_mail_1 = false;
+  $add_mail_2 = false;
+  $add_mail_3 = false;
+  $add_mail_4 = false;
+
 
   // Handler 
   if (empty($league_name)) {
     $messages['error'][] = 'Il manque votre nom d équipe';
     $_POST['league_name'] = 'Il manque votre nom d équipe';
   }
-
-  if (empty($friend_name)) {
+  elseif (!empty($league_name))
+  {
+    foreach ($leagues as $_league) {
+      if ($league_name === $_league->name) {
+        $messages['error'][] = 'Le nom de l équipe existe déjà';
+      }
+    }
+  }
+  
+  if (empty($friend_mail_1)) {
     $messages['error'][] = 'Il manque le mail de votre ami';
-    $_POST['friend_name'] = 'Il manque le mail de votre ami';
+    $_POST['friend_mail_1'] = 'Il manque le mail de votre ami';
+  }
+  elseif (!empty($friend_mail_1)) {
+    foreach ($users as $_user) {
+      if ($friend_mail_1 === ($_user->mail)) {
+        $add_mail_1 = true;
+      }
+    }
   }
 
+  if (!empty($friend_mail_2)) {
+    foreach ($users as $_user) {
+      if ($friend_mail_2 === ($_user->mail)) {
+        $add_mail_2 = true;
+      }
+    }
+  }
+
+  if (!empty($friend_mail_3)) {
+    foreach ($users as $_user) {
+      if ($friend_mail_3 === ($_user->mail)) {
+        $add_mail_3 = true;
+      }
+    }
+  }
+
+  if (!empty($friend_mail_4)) {
+    foreach ($users as $_user) {
+      if ($friend_mail_4 === ($_user->mail)) {
+        $add_mail_4 = true;
+      }
+    }
+  }
+
+  // Success
+  if ((empty($messages['error'])) && ($add_mail_1 == true) && ($add_mail_2 == false) && ($add_mail_3 == false) && ($add_mail_4 == false)) {
+
+    echo '<pre>';
+    print_r($friend_mail_1);
+    echo '</pre>';
+    echo('ta mere');
+
+    $prepare = $pdo->prepare('INSERT INTO leagues (name) VALUES (:name)');
+    $prepare->bindValue('name', $league_name);
+    $prepare->execute();
+
+    $prepare = $pdo->prepare('SELECT id FROM users WHERE users.mail = :mail');
+    $prepare->bindValue('mail', $friend_mail_1);
+    $prepare->execute();
+
+    $userId = $prepare->fetch()->id;
+
+    echo '<pre>';
+    print_r($userId);
+    echo '</pre>';
+
+    $prepare = $pdo->prepare('SELECT id FROM leagues WHERE leagues.name = :name');
+    $prepare->bindValue('name', $league_name);
+    $prepare->execute();
+
+    $leagueId = $prepare->fetch()->id;
+
+    echo '<pre>';
+    print_r($leagueId);
+    echo '</pre>';
 
 
- 
-  echo '<pre>';
-  print_r($_POST['friend_name']);
-  echo '</pre>';
+    $prepare = $pdo->prepare('INSERT INTO league_users (id_league, id_user) VALUES (:league, :user)');
+    $prepare->bindValue('league', $leagueId);
+    $prepare->bindValue('user', $userId);
+    $prepare->execute();
+
+
+    
+  }
+  if ((empty($messages['error'])) && ($add_mail_1 == true) && ($add_mail_2 == true) && ($add_mail_3 == false) && ($add_mail_4 == false)) {
+
+    echo '<pre>';
+    print_r($friend_mail_1);
+    echo '</pre>';
+    echo '<pre>';
+    print_r($friend_mail_2);
+    echo '</pre>';
+
+    echo('ton père');
+    
+  }
+  if ((empty($messages['error'])) && ($add_mail_1 == true) && ($add_mail_2 == true) && ($add_mail_3 == true) && ($add_mail_4 == false)) {
+
+    echo '<pre>';
+    print_r($friend_mail_1);
+    echo '</pre>';
+    echo '<pre>';
+    print_r($friend_mail_2);
+    echo '</pre>';
+    echo '<pre>';
+    print_r($friend_mail_3);
+    echo '</pre>';
+    
+    echo('ta soeur');
+  }
+  if ((empty($messages['error'])) && ($add_mail_1 == true) && ($add_mail_2 == true) && ($add_mail_3 == true) && ($add_mail_4 == true)) {
+
+    echo '<pre>';
+    print_r($friend_mail_1);
+    echo '</pre>';
+    echo '<pre>';
+    print_r($friend_mail_2);
+    echo '</pre>';
+    echo '<pre>';
+    print_r($friend_mail_3);
+    echo '</pre>';
+    echo '<pre>';
+    print_r($friend_mail_4);
+    echo '</pre>';
+    
+    echo('ton frère');
+
+  }
 }
 
 // Form not sent 
 else 
 {
-  $_POST['friend_name'] = '';
+  $_POST['friend_mail_1'] = '';
+  $_POST['friend_mail_2'] = '';
+  $_POST['friend_mail_3'] = '';
+  $_POST['friend_mail_4'] = '';
   $_POST['league_name'] = '';
 }
--- views/pages/league.php
+++ views/pages/league.php
@@ -12,6 +12,20 @@
         <h2 class="title">Créer ta ligue</h2>
       </div>
     </section>
+    
+    <div class="container--message">
+    <?php foreach($messages['error'] as $_message): ?>
+      <div class="message--error">
+        <?= $_message ?>
+      </div>
+    <?php endforeach; ?>
+    <?php foreach($messages['success'] as $_message): ?>
+      <div class="message--success">
+        <?= $_message ?>
+      </div>
+    <?php endforeach; ?>
+  </div>
+
     <section class="section--2">
       <div class="container--form">
         <h3 class="title--form">Nouvelle league</h3>
@@ -24,21 +38,21 @@
             Saisis l’adresse mail ou le pseudo de tes amis pour les inviter à rejoindre ta league :
           </p>
           <div class="field">
-            <label class="js-label" for="friend_name_1">Mail de ton ami</label>
-            <input class="input js-input" type="text" name="friend_name_1" value="<?= $_POST['friend_name_1']?>">
+            <label class="js-label" for="friend_mail_1">Mail de ton ami</label>
+            <input class="input js-input" type="text" name="friend_mail_1" value="<?= $_POST['friend_mail_1']?>">
           </div>
           <div class="field">
-            <label class="js-label" for="friend_name_2">Mail de ton ami</label>
-            <input class="input js-input" type="text" name="friend_name_2" value="<?= $_POST['friend_name_2']?>">
+            <label class="js-label" for="friend_mail_2">Mail de ton ami</label>
+            <input class="input js-input" type="text" name="friend_mail_2" value="<?= $_POST['friend_mail_2']?>">
           </div>
           <div class="field--new js-new-field">
             <div class="field hidden js-field-1">
-              <label class="js-label" for="friend_name_3">Mail de ton ami</label>
-              <input class="input js-input" type="text" name="friend_name_3" value="<?= $_POST['friend_name_3']?>">
+              <label class="js-label" for="friend_mail_3">Mail de ton ami</label>
+              <input class="input js-input" type="text" name="friend_mail_3" value="<?= $_POST['friend_mail_3']?>">
             </div>
             <div class="field hidden js-field-2">
-              <label class="js-label" for="friend_name_4">Mail de ton ami</label>
-              <input class="input js-input" type="text" name="friend_name_4" value="<?= $_POST['friend_name_4']?>">
+              <label class="js-label" for="friend_mail_4">Mail de ton ami</label>
+              <input class="input js-input" type="text" name="friend_mail_4" value="<?= $_POST['friend_mail_4']?>">
             </div>
           </div>
           <div class="button--add js-add-friend">
