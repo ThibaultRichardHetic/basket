39c772b9522fdff1152d45963874b8e7b16f6d55
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2019-04-12 04:49:07 +0200

    :sparkles: get api for player img

--- views/pages/club.php
+++ views/pages/club.php
@@ -69,9 +69,101 @@ $urlAvatarVs = 'https://avatars.dicebear.com/v2/male/:'.$GLOBALS['id_vs'].'.svg'
       <!-- <div class="container--img">
       </div> -->
       <div class="container--team">
+        <?php 
+          $prepare = $pdo->prepare('SELECT id_player FROM league_users WHERE ((id_user = :user) && (id_league = :league))');
+          $prepare->bindValue('league', $leagueId);
+          $prepare->bindValue('user', $_SESSION['id']);
+          $prepare->execute();
+          $id_player = $prepare->fetch()->id_player;
+
+
+          $prepare = $pdo->prepare('SELECT player_1, player_2, player_3, player_4, player_5 FROM players WHERE id = 4');
+          $prepare->bindValue('id', $id_player);
+          $prepare->execute();
+          $players = $prepare->fetchAll();
+
+          // echo '<pre>';
+          // print_r($players);
+          // echo '</pre>';
+
+          function callImg($name){
+            // Get cURL resource
+            $ch = curl_init();
+
+            // Set url
+            $url = 'https://api.mysportsfeeds.com/v1.2/pull/nba/2018-2019-regular/active_players.json?player=';
+            $url = $url.$name;
+
+            curl_setopt($ch, CURLOPT_URL, $url);
+
+            // Set method
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+            // Set options
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+            // Set compression
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+            // Set headers
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+              "Authorization: Basic " . base64_encode('9ecc103f-42b8-4703-a90e-3110b7' . ":" . 'hetic2019')
+            ]);
+
+            // Send the request & save response to $resp
+            $resp = curl_exec($ch);
+
+            $resp = json_decode($resp);
+
+            if (!$resp) {
+              die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
+            }
+
+            // Close request to clear up some resources
+            curl_close($ch);
+
+
+            $img = $resp->activeplayers->playerentry[0]->player->officialImageSrc;
+            return $img;
+          }
+
+
+        ?>
         <h4 class="title">Ton équipe</h4>
         <div class="container--players">
-          lfe,fel
+          <div class="player">
+            <h4 class="name"><?= $players[0]->player_1?></h4>
+
+            <div class="container--img">
+              <?php $name = $players[0]->player_1 ?>
+              <img src="<?= callImg($name); ?>" alt="">
+            </div>
+          </div>
+          <div class="player">
+            <h4 class="name"><?= $players[0]->player_2?></h4>
+            <div class="container--img">
+              <img src="" alt="">
+            </div>
+          </div>
+          <div class="player">
+            <h4 class="name"><?= $players[0]->player_3?></h4>
+            <div class="container--img">
+              <img src="" alt="">
+            </div>
+          </div>
+          <div class="player">
+            <h4 class="name"><?= $players[0]->player_4?></h4>
+            <div class="container--img">
+              <img src="" alt="">
+            </div>
+          </div>
+          <div class="player">
+            <h4 class="name"><?= $players[0]->player_5?></h4>
+            <div class="container--img">
+              <img src="" alt="">
+            </div>
+          </div>
         </div>
       </div>
     </section>
